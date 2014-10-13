e73e291af461777a15e83f7f89cc5b1d6958b4cb
Matthew Fenelon <matt@fnln.me>
2014-10-13 14:19:53 +0100

    Fix the generator not being able to find its templates.

--- lib/generators/spree_simple_html_emails/install/install_generator.rb
+++ lib/generators/spree_simple_html_emails/install/install_generator.rb
@@ -1,6 +1,8 @@
 module SpreeSimpleHtmlEmails
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
       def add_stylesheets
         copy_file 'stylesheets/spree_simple_html_emails.css.scss', 'app/assets/stylesheets/store/spree_simple_html_emails.css.scss'
       end
